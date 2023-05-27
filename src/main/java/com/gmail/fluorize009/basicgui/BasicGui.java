361dc09141f99a5eafd9e1193d748a6e6486ab45
Fluorize <supergame314@gmail.com>
2023-05-27 21:57:33 +0900

    initial commit

---
+++ src/main/java/com/gmail/fluorize009/basicgui/BasicGui.java
@@ -0,0 +1,17 @@
+package com.gmail.fluorize009.basicgui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BasicGui extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
