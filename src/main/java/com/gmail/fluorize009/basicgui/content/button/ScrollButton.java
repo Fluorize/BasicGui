dcfe8ee8d4bcf69c2891ceacdd927fb4c8a87321
Fluorize <fluorizemc@gmail.com>
2023-05-29 22:35:21 +0900

    updated ScrollButton.java

--- src/main/java/com/gmail/fluorize009/basicgui/content/button/ScrollButton.java
+++ src/main/java/com/gmail/fluorize009/basicgui/content/button/ScrollButton.java
@@ -1,27 +1,56 @@
 package com.gmail.fluorize009.basicgui.content.button;
 
+import com.gmail.fluorize009.basicgui.holder.ContentHolder;
 import com.gmail.fluorize009.basicgui.holder.ContentList;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class ScrollButton extends GuiButton{
 
-    private final ContentList target;
+    private final ContentHolder target;
+    private final ScrollType type;
 
-    public ScrollButton(ItemStack icon, ContentList target) {
+    public ScrollButton(ItemStack icon, ContentHolder target,ScrollType type) {
         super(icon);
+        if(!type.getHolderClass().isInstance(target)){
+            throw new IllegalArgumentException();
+        }
         this.target = target;
+        this.type = type;
     }
 
 
     @Override
     public boolean perform(Player performer) {
-        target.goNext();
+        switch (type){
+            case NEXT:
+                ((ContentList) target).goNext();
+                break;
+            case BACK:
+                ((ContentList) target).goBack();
+                break;
+        }
         return false;
     }
 
     @Override
     public boolean isCloseTrigger() {
         return false;
+    }
+
+
+    public enum ScrollType{
+        NEXT(ContentList.class),
+        BACK(ContentList.class);
+
+        private final Class<?> holderClass;
+
+        ScrollType(Class<?> holderClass){
+            this.holderClass = holderClass;
+        }
+
+        public Class<?> getHolderClass() {
+            return holderClass;
+        }
     }
 }
